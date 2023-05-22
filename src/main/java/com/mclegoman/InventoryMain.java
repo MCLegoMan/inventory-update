3f268af8f3d5113cbec18fdf39bb6d64fce2f729
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2023-05-23 04:59:40 +1200

    1.1.3 - Fixed #1. Removed setMaxCount, Added getMaxCount mixin to set values.

--- src/main/java/com/mclegoman/InventoryMain.java
+++ src/main/java/com/mclegoman/InventoryMain.java
@@ -1,20 +1,11 @@
 package com.mclegoman;
 
 import com.mclegoman.config.InventoryConfig;
-import com.mclegoman.mixin.InventoryItem;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.item.Item;
-import net.minecraft.registry.Registries;
 
 public class InventoryMain implements ModInitializer {
     @Override
     public void onInitialize() {
         InventoryConfig.registerConfigs();
-        for (Item item : Registries.ITEM) {
-            if (item.getMaxCount() != 1) {
-                if (item.getMaxCount() == 16) ((InventoryItem) item).setMaxCount(InventoryConfig.MAX_STACK_MINI);
-                else ((InventoryItem) item).setMaxCount(InventoryConfig.MAX_STACK_DEFAULT);
-            }
-        }
     }
 }
---
+++ src/main/java/com/mclegoman/mixin/InventoryItemStackSize.java
@@ -0,0 +1,22 @@
+package com.mclegoman.mixin;
+
+import com.mclegoman.config.InventoryConfig;
+import net.minecraft.item.Item;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Item.class)
+public abstract class InventoryItemStackSize {
+    @Shadow @Final private int maxCount;
+    @Inject(method = "getMaxCount", at = @At("RETURN"), cancellable = true)
+    private void getMaxCount(CallbackInfoReturnable<Integer> cir) {
+        if (maxCount != 1) {
+            if (maxCount == 16) cir.setReturnValue(InventoryConfig.MAX_STACK_MINI);
+            cir.setReturnValue(InventoryConfig.MAX_STACK_DEFAULT);
+        }
+    }
+}
