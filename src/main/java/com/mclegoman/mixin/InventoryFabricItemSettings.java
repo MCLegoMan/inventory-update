efe36dc6c834c2b315edf413a871225dbf71b6dd
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2023-05-23 03:08:33 +1200

    1.1.0 - Fixed Issue #1, Non-Vanilla Items now use the max stack value.

--- src/main/java/com/mclegoman/mixin/InventoryFabricItemSettings.java
+++ src/main/java/com/mclegoman/mixin/InventoryFabricItemSettings.java
@@ -1,14 +1,16 @@
 package com.mclegoman.mixin;
 
 import com.mclegoman.config.InventoryConfig;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-@Mixin(value = Item.Settings.class)
-public abstract class InventoryItemSettings {
-    @ModifyVariable(method = "maxCount", at = @At("RETURN"), argsOnly = true)
+@Mixin(value = FabricItemSettings.class)
+public abstract class InventoryFabricItemSettings {
+    @ModifyVariable(method = "maxCount(I)Lnet/minecraft/item/Item$Settings;", at = @At("RETURN"), argsOnly = true)
     private int maxCount(int maxCount) {
         if (maxCount != 1) {
             if (maxCount == 16) return InventoryConfig.MAX_STACK_MINI;
